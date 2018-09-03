889764e33c90ab7684931b54110a1f4a03f12197
luoromeo <imy091105>
2018-09-03 21:35:18 +0800

    1、添加core java 代码

---
+++ src/com/luoromeo/study/stream/CollectingResults.java
@@ -0,0 +1,64 @@
+package com.luoromeo.study.stream;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectingResults {
+
+    public static Stream<String> noVowels() throws IOException {
+        String contents = new String(Files.readAllBytes(Paths.get("alice30.txt")), StandardCharsets.UTF_8);
+        List<String> wordList = Arrays.asList(contents.split("\\PL+"));
+        Stream<String> words = wordList.stream();
+        return words.map(s -> s.replaceAll("[aeiouAEIOU]", ""));
+    }
+
+    public static <T> void show(String label, Set<T> set) {
+        System.out.print(label + ": " + set.getClass().getName());
+        System.out.println("[" + set.stream().limit(10).map(Objects::toString).collect(Collectors.joining(", ")) + "]");
+    }
+
+    public static void main(String[] args) throws IOException {
+        Iterator<Integer> iter = Stream.iterate(0, n -> n + 1).limit(10).iterator();
+        while (iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+        Object[] numbers = Stream.iterate(0, n -> n + 1).limit(10).toArray();
+        System.out.println("Object array:" + Arrays.toString(numbers));
+
+        try {
+            Integer number = (Integer) numbers[0];
+            System.out.println("number:" + number);
+            System.out.println("The following statement throws an exception");
+            Integer[] numbers2 = (Integer[]) numbers; // error
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+
+        Integer[] numbers3 = Stream.iterate(0, n -> n + 1).limit(10).toArray(Integer[]::new);
+        System.out.println("Integer array:" + Arrays.toString(numbers3));
+
+        Set<String> noVowelSet = noVowels().collect(Collectors.toSet());
+        show("noVoweSet", noVowelSet);
+
+        TreeSet<String> noVowelTreeSet = noVowels().collect(Collectors.toCollection(TreeSet::new));
+        show("noVowelTreeSet", noVowelTreeSet);
+
+        String result = noVowels().limit(10).collect(Collectors.joining());
+        System.out.println("Joinging: " + result);
+
+        result = noVowels().limit(10).collect(Collectors.joining(","));
+        System.out.println("Joinging with commas: " + result);
+
+        IntSummaryStatistics summary = noVowels().collect(Collectors.summarizingInt(String::length));
+        double averageWordLength = summary.getAverage();
+        double maxWordLength = summary.getMax();
+        System.out.println("Average word length: " + averageWordLength);
+        System.out.println("Max word length:" + maxWordLength);
+        noVowels().limit(10).forEach(System.out::println);
+    }
+}
